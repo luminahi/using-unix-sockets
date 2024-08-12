3e8ac2f7c1cc5fcb49a2bd3af88987bfd57f961d
Alex Lima <delimaferreiraalex@gmail.com>
2024-08-11 23:47:51 -0300

    ref: improve code
    remove unnecessary includes
    combine function calls in a big function

--- src/client.c
+++ src/client.c
@@ -1,28 +1,28 @@
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "socket.h"
 
-int main(int argc, char* argv[]) {
-    if (argc <= 1) return 0;
+#define FILEPATH "/tmp/custom.sock"
+#define BUFFER_SIZE 128
 
-    char buffer[128];
-
+void publish(char* file_path, int buffer_size, char* message) {
+    char buffer[buffer_size];
     int socket_fd;
     struct sockaddr_un address;
 
-    create_socket(&socket_fd, &address, "/tmp/mysocket");
+    create_socket(&socket_fd, &address, file_path);
     connect_socket(socket_fd, &address);
 
-    send_message(socket_fd, buffer, argv[1], strlen(argv[1]));
-    receive_message(socket_fd, buffer, 128);
+    send_message(socket_fd, buffer, message, strlen(message));
+    receive_message(socket_fd, buffer, buffer_size);
 
     printf("Server message: %s\n", buffer);
     close(socket_fd);
+}
+
+int main(int argc, char* argv[]) {
+    publish(FILEPATH, BUFFER_SIZE, argv[1] ? argv[1] : "no message");
 
     return 0;
 }

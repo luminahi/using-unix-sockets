f51c45f4a13a0f23626285add8697391c13d72a3
Alex Lima <delimaferreiraalex@gmail.com>
2024-09-01 23:10:49 -0300

    ref: move socket module to a folder above

---
+++ src/lib/socket.c
@@ -0,0 +1,84 @@
+#include <sys/un.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "socket.h"
+
+int assert_socket(char* filepath, int type, struct sockaddr_un* address) {
+    int socket_fd;
+    
+    socket_fd = socket(AF_UNIX, type, 0);
+    if (socket_fd == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(address, 0, sizeof(struct sockaddr_un));
+    address->sun_family = AF_UNIX;
+    strncpy(address->sun_path, filepath, sizeof(address->sun_path) - 1);
+
+    return socket_fd;
+}
+
+void connect_socket(int socket_fd, struct sockaddr_un* address) {
+    if (connect(socket_fd, (struct sockaddr*)address, sizeof(struct sockaddr_un)) == -1) {
+        perror("connect");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void bind_socket(int socket_fd, struct sockaddr_un* address) {
+    if (bind(socket_fd, (struct sockaddr*)address, sizeof(struct sockaddr_un)) == -1) {
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void listen_socket(int socket_fd) {
+    if (listen(socket_fd, 5) == -1) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int accept_connection(int server_socket_fd) {
+    int client_socket_fd = accept(server_socket_fd, NULL, NULL);
+    if (client_socket_fd == -1) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+    return client_socket_fd;
+}
+
+void receive_message_stream(int socket_fd, char* buffer, int buffer_length) {
+    if (recv(socket_fd, buffer, buffer_length, 0) == -1) {
+        perror("recv");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int receive_message_dgram(int socket_fd, char* buffer, int buffer_size, struct sockaddr_un* addr, int* addr_len) {
+    int num_bytes = recvfrom(socket_fd, buffer, buffer_size, 0, (struct sockaddr*)addr, (socklen_t*)addr_len);
+    if (num_bytes == -1) {
+        perror("recvfrom");
+        exit(EXIT_FAILURE);
+    }
+
+    return num_bytes;
+}
+
+void send_message_stream(int socket_fd, char* buffer, char* message, int message_length) {
+    strcpy(buffer, message ? message : "default message");
+    
+    if (send(socket_fd, buffer, message_length, 0) == -1) {
+        perror("send");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void send_message_dgram(int socket_fd, char* buffer, struct sockaddr* addr) {
+    sendto(socket_fd, buffer, strlen(buffer), 0, (struct sockaddr*)addr, sizeof(struct sockaddr));
+    perror("sendto");
+}
---
+++ src/lib/socket.h
@@ -0,0 +1,20 @@
+#ifndef _SOCKET_H
+#define _SOCKET_H
+
+int assert_socket(char*, int, struct sockaddr_un*);
+
+void connect_socket(int, struct sockaddr_un*);
+
+void bind_socket(int, struct sockaddr_un*);
+
+void listen_socket(int);
+
+int accept_connection(int);
+
+void receive_message_stream(int, char*, int);
+
+int receive_message_dgram(int, char*, int, struct sockaddr_un*, int*);
+
+void send_message_stream(int, char*, char*, int);
+
+#endif
